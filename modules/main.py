7d2dadfee5309b99e8e958c548d146dce7bc976a
Jason <jason.peterson03@gmail.com>
2022-01-23 21:16:04 -0800

    add search name button and field

--- modules/main.py
+++ modules/main.py
@@ -15,12 +15,28 @@ class TabFrame(ttk.Notebook):
     def __init__(self,container):
         super().__init__(container)
         
+        self.grid(column=0,row=1,columnspan=2,rowspan=2,sticky=tk.SW)
         #options = {'width':550,'height'}
-        ttk.Frame(self,width=550,height=280).add(0,text='Set Management')
-        ttk.Frame(self,width=550,height=280).add(1,text='Deck Management')
-        ttk.Frame(self,width=550,height=280).add(2,text='Database Search')
-        ttk.Frame(self,width=550,height=280).add(3,text='Result List')
+        page1 = ttk.Frame(self,width=550,height=280)
+        self.add(page1,text='Set Management')
+        page2 = ttk.Frame(self,width=550,height=280)
+        self.add(page2,text='Deck Management')
+        page3 = ttk.Frame(self,width=550,height=280)
+        self.add(page3,text='Database Search')
+        page4 = ttk.Frame(self,width=550,height=280)
+        self.add(page4,text='Result List')
 
+        self.searchVar = tk.StringVar()
+
+        self.name_search_btn = ttk.Button(page3,text='Name Search')
+        self.name_search_btn['command'] = self.search_name
+        self.name_search_btn.grid(column=0,row=1,columnspan=1,rowspan=1,sticky=tk.W)
+        
+        self.search_text = ttk.Entry(page3,textvariable=self.searchVar)
+        self.search_text.grid(column=1,row=1,columnspan=1,rowspan=1,sticky=tk.W,padx=5, pady=5)
+
+    def search_name(self):
+        pass
 
 class App(tk.Tk):
     def __init__(self):
@@ -29,6 +45,11 @@ class App(tk.Tk):
         self.title('Deck Manager - Gen 2')
         self.geometry('700x450+300+40')
         self.resizable(False, False)
+        # quit button
+        self.quit_button = ttk.Button(self,text='Quit')
+        self.quit_button['command'] = lambda: self.quit()
+        self.quit_button.grid(column=0,row=0,columnspan=1,rowspan=1,sticky=tk.W)
+
 
 if __name__ == "__main__":
     app = App()
